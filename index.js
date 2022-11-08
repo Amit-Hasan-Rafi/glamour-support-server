974613f4099911c49ff0b0e01b63b3ab94cf59e3
Amit-Hasan-Rafi <amithasanrafe1999@gmail.com>
2022-11-09 03:22:46 +0600

    service data push

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -11,13 +11,48 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gzgfqcq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
+
+async function run() {
+    try {
+        const productsCollections = client.db('Glamour-support-products').collection('products')
+        const servicesCollections = client.db('Glamour-support-products').collection('services')
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productsCollections.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const products = await productsCollections.findOne(query)
+            res.send(products)
+        })
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollections.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const products = await servicesCollections.findOne(query)
+            res.send(products)
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(err => console.log(err))
 
 
-app.get('/', (req , res) =>{
+app.get('/', (req, res) => {
     res.send('NODE.js running')
 })
 
-app.listen(port , ()=>{
+app.listen(port, () => {
     console.log(`port running ${port}`)
 })
